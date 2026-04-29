2afaab5ff86d03040b85c8024c3959c5f90546ec
phuc1314 <phucha110706@gmail.com>
2026-04-29 09:04:39 +0700

    commit main

--- main.cpp
+++ main.cpp
@@ -6,11 +6,58 @@
 #include "LinkedList.h"
 #include <iomanip>
 #include <ctime>
+
 using namespace std;
 
 ThongTinXe** p;
+int soHang = 0, soCot = 0;
 HashTable ht(101);
 LinkedList dsChoTrong;
+
+string sinhBienSo() {
+    return to_string(rand() % 90 + 10) + "A-" + to_string(rand() % 90000 + 10000);
+}
+
+// sinh dữ liệu test
+void sinhData(string tenFile, int n, int m, int soLuong) {
+    ofstream file(tenFile);
+
+    if (!file) {
+        cout << "Loi mo file!\n";
+        return;
+    }
+
+    file << n << " " << m << endl;
+
+    for (int i = 0; i < soLuong; i++) {
+        string bs = sinhBienSo();
+        char hang = 'A' + rand() % n;
+        int vitri = rand() % m + 1;
+        long long t = 1700000000 + rand() % 100000;
+
+        file << bs << " " << hang << " " << vitri << " " << t << endl;
+    }
+
+    file.close();
+}
+
+void ghiFile(string tenFile) {
+    ofstream file(tenFile);
+    file << soHang << " " << soCot << endl;
+
+    for (int i = 0; i < soHang; i++) {
+        for (int j = 0; j < soCot; j++) {
+            if (p[i][j].getBienSo() != "") {
+                file << p[i][j].getBienSo() << " "
+                    << char('A' + i) << " "
+                    << j + 1 << " "
+                    << p[i][j].getTimeIn() << endl;
+            }
+        }
+    }
+
+    file.close();
+}
 
 long long tinhTien(long long in, long long out) {
     long long d = out - in;
@@ -41,22 +88,27 @@ void traXe() {
         cout << "Khong tim thay xe!\n";
         return;
     }
+    long long timeIn = xe.getTimeIn();
+    time_t t = timeIn;
+    tm* in = localtime(&t);
 
     long long timeOut = time(0);
+    time_t t2 = timeOut;
+    tm* out = localtime(&t2);
 
     long long tien = tinhTien(xe.getTimeIn(), timeOut);
 
     cout << "\n===== HOA DON =====\n";
     cout << "Bien so: " << xe.getBienSo() << endl;
     cout << "Vi tri: " << xe.getHang() << xe.getViTri() << endl;
-    cout << "Thoi gian vao: " << xe.getTimeIn() << endl;
-    cout << "Thoi gian ra: " << timeOut << endl;
+    cout << "Thoi gian vao: " << in->tm_hour << ":" << in->tm_min << ":" << in->tm_sec  << endl;
+    cout << "Thoi gian ra: " << out->tm_hour << ":" << out->tm_min << ":" << out->tm_sec << endl;
     cout << "Tien: " << tien << endl;
 
     ht.remove(bs);
-
     p[xe.getHang() - 'A'][xe.getViTri() - 1] = ThongTinXe();
     dsChoTrong.addSorted(xe.getHang(), xe.getViTri());
+    ghiFile("DemoData_MaNhom.txt");
     cout << "Tra xe thanh cong!\n";
 }
 
@@ -95,7 +147,7 @@ void guiXe() {
     p[hang - 'A'][viTri - 1] = xe;
 
     // 7. ghi file 
-    ghiFile("data.txt");
+    ghiFile("DemoData_MaNhom.txt");
 
     // 8. tgian dang HH:MM:SS
     time_t t = timeIn;
@@ -111,53 +163,6 @@ void guiXe() {
         << endl;
 }
 
-void ghiFile(string tenFile) {
-    ofstream file(tenFile);
-
-    int n = 5, m = 5; // chỉnh theo bãi xe nếu cần
-
-    file << n << " " << m << endl;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (p[i][j].getBienSo() != "") {
-                file << p[i][j].getBienSo() << " "
-                     << char('A' + i) << " "
-                     << j + 1 << " "
-                     << p[i][j].getTimeIn() << endl;
-            }
-        }
-    }
-
-    file.close();
-    
-    // sinh biển số random
-string sinhBienSo() {
-    return to_string(rand() % 90 + 10) + "A-" + to_string(rand() % 90000 + 10000);
-}
-
-// sinh dữ liệu test
-void sinhData(string tenFile, int n, int m, int soLuong) {
-    ofstream file(tenFile);
-
-    if (!file) {
-        cout << "Loi mo file!\n";
-        return;
-    }
-
-    file << n << " " << m << endl;
-
-    for (int i = 0; i < soLuong; i++) {
-        string bs = sinhBienSo();
-        char hang = 'A' + rand() % n;
-        int vitri = rand() % m + 1;
-        long long t = 1700000000 + rand() % 100000;
-
-        file << bs << " " << hang << " " << vitri << " " << t << endl;
-    }
-
-    file.close();
-}
 int main() {
 
     return 0;
